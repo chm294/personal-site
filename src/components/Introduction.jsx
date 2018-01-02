b4c307ec5d676eded0a3d9587c6328eea44b840b
Christine <christinehma214@gmail.com>
2018-01-02 13:15:19 -0500

    minor edits to intro

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -9,10 +9,16 @@ export default class Introduction extends React.Component {
 	render() {
 		return (
 			<div className="content-container body-text">
-				<p>Hello, my name is Christine Ma, and I am a full stack software developer.</p>
-				<br/>
-				<p>I studied at NYU majoring in Art History and hold a minor in Urban Design. 
-				My interests include cooking, <a href="https://vimeo.com/user53596363" target="_blank">fiddling with 2-D animation </a>, and digital design.</p> 
+				<p>Hello, my name is Christine Ma, and I am a full stack software engineer.
+				<br/><br/>
+				I studied at NYU majoring in Art History and hold a minor in Urban 
+				Design. My interests include cooking, digital design, 
+				<a href="https://vimeo.com/user53596363" target="_blank"> fiddling with 2-D animation</a>, 
+				and learning about painting conservation. 
+				<br/><br/>
+				If you have a job opportunity or lead to my next expedition, let's chat over coffee! 
+				<p className="important-text">christinehma214@gmail.com </p>
+				</p>
 				<div className="contact-footer">
 					<a href="https://www.linkedin.com/in/christinehma/" target="_blank">
 						<img src="https://www.usplastic.com/mobile/images/icons/social/linkedin.svg" className="icon" alt="LinkedIn"/> 
